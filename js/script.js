4a76d56a94c2b710c1a392a7cf4f7588e92cfac4
revathipanyam <112520998+revathipanyam@users.noreply.github.com>
2022-11-07 13:27:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  function nameAlert(){
+   var userInput = document.getElementById('entryinput').value;
+    alert("Revathi Panyam: " + userInput);
+   var alertOutput = document.getElementById('textoutput');
+    alertOutput.innerHTML = userInput;
+   //call the function 'nameAlert' when we click the alert
+    var alertButton = document.getElementById('entrybutton');
+  alertButton.addEventListener('click',nameAlert);
+}
 window.addEventListener('load', init);
